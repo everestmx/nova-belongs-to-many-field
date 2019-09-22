63fd7d4401fe87c13c77e2f18ec70d1f13ee1729
Dmitry Sergeev <dsidorov@hypermetrica.ru>
2019-09-22 17:48:34 +0300

    Fix multiselect style disabled class

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -8,32 +8,18 @@
 </template>
 
 <script>
-export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
+	export default {
+		props: ['resource', 'resourceName', 'resourceId', 'field'],
 
-    computed: {
-		values() {
-			if (!this.field.value) {
-				return;
-            }
+		computed: {
+			values()
+			{
+				if (!this.field.value) {
+					return;
+				}
 
-			return this.field.value.map(val => val[this.field.optionsLabel || 'name']).filter(val => !!val);
+				return this.field.value.map(val => val[this.field.optionsLabel || 'name']).filter(val => !!val);
+			},
 		},
-    }
-}
+	};
 </script>
-
-<style>
-    .btmf_item {
-        color: #fff;
-        position: relative;
-        display: inline-block;
-        padding: 2px 10px 2px 10px;
-        margin: 0 0 5px 5px;
-        border-radius: 3px;
-        background: #41b883;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        font-size: 14px;
-    }
-</style>
